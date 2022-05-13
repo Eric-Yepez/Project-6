613f4a30bc34e0a31c838f9ff5894cc8bfbf745e
Eric-Yepez <eri2260169@maricopa.edu>
2022-05-12 23:50:54 -0700

    Aggregator Controller Update but sill has errors

--- project-6-initial-master/aggregator/src/main/java/com/example/aggregator/controller/AggregatorController.java
+++ project-6-initial-master/aggregator/src/main/java/com/example/aggregator/controller/AggregatorController.java
@@ -1,5 +1,6 @@
 package com.example.aggregator.controller;
 
+import com.example.aggregator.client.AggregatorRestClient;
 import com.example.aggregator.model.Entry;
 import com.example.aggregator.service.AggregatorService;
 import org.slf4j.Logger;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -31,8 +33,32 @@ public class AggregatorController {
     }
 
     @GetMapping("/getAllPalindromes/{word}")
-    public<List> Entry getAllPalindromes(@PathVariable String word) {
+    public List<int> getAllPalindromes() {
 
-        return service.getDefinitionFor(word);
+        static List<int> candidates = new ArrayList<>();
+
+        for(int i=1;i<=size;i++)
+        {
+            List<Entry> startsWith = restClient.getWordsStartingWith(c);
+            List<Entry> endsWith = restClient.getWordsEndingWith(c);
+            List<Entry> start&EndsWith = new ArrayList<>(startsWith);
+            starts&EndsWith.retainAll(endsWith);
+            candidates.addAll(starts&EndsWith);
+        });
+        return candidates.stream()
+        void reverse(int n)
+        {
+            int reverse= 0;
+
+            while (n!=0) {
+
+                int reverse = reverse * 10 + n % 10;
+
+                n = n/10;}
+
+            return reverse;
+
+        }
     }
 }
+
